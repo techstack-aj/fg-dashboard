5473db3e3ddeb5c4f3ed3b7b67bb401c2e22ec79
techstack-aj <237093328+techstack-aj@users.noreply.github.com>
2025-11-27 13:06:00 +0100

    chore: cleanup comments

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
-// TODO: Importiere deine Komponenten (IndexDetail, NotFound)
+// TODO: Importiere deine Komponenten (IndexDetail)
 import AppMUI from './App-MUI';
 import App from './App';
 import Navigation from './components/Navigation';
