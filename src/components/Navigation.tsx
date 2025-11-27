7a735c3d7972a6ece3306a43da76896ceee3c6fd
techstack-aj <237093328+techstack-aj@users.noreply.github.com>
2025-11-27 01:27:28 +0100

    feat: setup roadmap and templates for chapter 2 router integration

---
+++ src/components/Navigation.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// TODO: Importiere Link oder NavLink von 'react-router-dom'
+
+const Navigation: React.FC = () => {
+  return (
+    <nav style={{ padding: '1rem', borderBottom: '1px solid #ccc', marginBottom: '1rem' }}>
+      {/* TODO: Erstelle Links zu '/' und '/mui' */}
+      {/* Beispiel: <Link to="/">Original</Link> */}
+      <span>Navigation Template - Bitte implementieren</span>
+    </nav>
+  );
+};
+
+export default Navigation;
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import AppOriginal from './App'
-import AppMUI from './App-MUI'
+// import AppOriginal from './App'
+// import AppMUI from './App-MUI'
 import './index.css'
 import { ThemeProvider } from './context/ThemeContext'
+// TODO: Importiere BrowserRouter von 'react-router-dom'
+// TODO: Importiere AppRouter (den du erstellt hast)
 
-// Lade Dashboard-Präferenz aus LocalStorage
-const useMUI = localStorage.getItem('dashboard-version') === 'mui';
-const App = useMUI ? AppMUI : AppOriginal;
+// ALTE LOGIK (Entfernen oder Auskommentieren):
+// const useMUI = localStorage.getItem('dashboard-version') === 'mui';
+// const App = useMUI ? AppMUI : AppOriginal;
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <ThemeProvider>
-            <App />
+            {/* TODO: Umschließe alles mit <BrowserRouter> */}
+            {/* TODO: Rendere <AppRouter /> statt <App /> */}
+            {/* <App />  <-- Das hier ersetzen */}
+            <div>Bitte main.tsx für Router konfigurieren (siehe Kommentare)</div>
         </ThemeProvider>
     </React.StrictMode>,
 )
