f95fb4d6a8e5d96eacf2e573e030755e4feb404a
techstack-aj <237093328+techstack-aj@users.noreply.github.com>
2025-11-27 02:11:36 +0100

    feat: setup react router in main.tsx (#15)

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import AppOriginal from './App'
-// import AppMUI from './App-MUI'
 import './index.css'
 import { ThemeProvider } from './context/ThemeContext'
-// TODO: Importiere BrowserRouter von 'react-router-dom'
-// TODO: Importiere AppRouter (den du erstellt hast)
-
-// ALTE LOGIK (Entfernen oder Auskommentieren):
-// const useMUI = localStorage.getItem('dashboard-version') === 'mui';
-// const App = useMUI ? AppMUI : AppOriginal;
+import { BrowserRouter } from 'react-router-dom'
+import AppRouter from './AppRouter'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <ThemeProvider>
-            {/* TODO: Umschließe alles mit <BrowserRouter> */}
-            {/* TODO: Rendere <AppRouter /> statt <App /> */}
-            {/* <App />  <-- Das hier ersetzen */}
-            <div>Bitte main.tsx für Router konfigurieren (siehe Kommentare)</div>
-        </ThemeProvider>
-    </React.StrictMode>,
+        <BrowserRouter>
+            <ThemeProvider>
+                <AppRouter />
+            </ThemeProvider>
+        </BrowserRouter>
+    </React.StrictMode>
 )
